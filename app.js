6b4a4d27470b130f748e03d368d9c89e20d90a13
pritam-sharma <pritamsharma531@gmail.com>
2024-12-13 16:31:19 +0530

    err parameter set in callback in app.js

--- app.js
+++ app.js
@@ -55,9 +55,10 @@ const store = MongoStore.create({
   touchAfter: 24 * 3600,
 });
 
-store.on("error", () => {
+store.on("error", (err) => {
   console.log("ERROR IN MONGO SESSION STORE", err);
 });
+
 const sessionOptions = {
   store,
   secret: process.env.SECRET,
